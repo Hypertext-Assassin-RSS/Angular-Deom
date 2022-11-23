3045b493031e19e0b35971a4bce9acfecd353e1d
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2022-11-23 17:00:56 +0530

    basic of template

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -2,12 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: '[app-demo]',
-  template: `<div> Sample Message</div>`,
+  template: `<div> {{title}}</div>
+  <p>{{myobject.material}}</p>
+  <ul>
+    <li *ngIf="myarr; else idTemplate">Array is not empty</li>
+    <li *ngFor = "let char of myarr1  ">{{char}}</li>
+  </ul>
+  <ng-template #idTemplate >Array is Empty</ng-template>
+  `,
   styles: [`div
   {
     color: red;
   }`]
 })
 export class DemoComponent {
-
+  title='app working!';
+  myobject = {
+    color:'red',
+    material:'soft'
+  };
+  myarr = false;
+  myarr1 = ['a','e','i','o','u'];
 }
