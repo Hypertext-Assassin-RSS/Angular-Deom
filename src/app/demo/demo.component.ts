cdbe90f91455478666511f7997e79cdf4c7f6ff5
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2022-11-23 14:42:52 +0530

    project update

---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '[app-demo]',
+  template: `<div> Sample Message</div>`,
+  styles: [`div
+  {
+    color: red;
+  }`]
+})
+export class DemoComponent {
+
+}
