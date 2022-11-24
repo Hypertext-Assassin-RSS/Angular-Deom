7778cbfd259ad43b5ab7d4904624db082143cca6
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2022-11-24 12:56:06 +0530

    basic angular routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule , routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DemoComponent } from './demo/demo.component';
 import { DataComponent } from './data/data.component';
 import {TestService} from "./test.service";
 
+
 @NgModule({
   declarations: [
     AppComponent,
     DemoComponent,
-    DataComponent
+    DataComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
